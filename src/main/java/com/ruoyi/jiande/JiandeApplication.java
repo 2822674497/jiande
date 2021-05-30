f9334311f8ab33fbc42744d59e64ec8d0f0be5e2
2822674497 <2822674497@qq.com>
2021-05-30 21:16:23 +0800

    第二个版本

--- src/main/java/com/ruoyi/jiande/JiandeApplication.java
+++ src/main/java/com/ruoyi/jiande/JiandeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JiandeApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(JiandeApplication.class, args);
     }
 
