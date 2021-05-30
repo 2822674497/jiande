9e7c375b5d5440fd4b40149bb0601c7f237bdc05
2822674497 <2822674497@qq.com>
2021-05-30 21:09:27 +0800

    第一个版本

---
+++ src/main/java/com/ruoyi/jiande/JiandeApplication.java
@@ -0,0 +1,13 @@
+package com.ruoyi.jiande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiandeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiandeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruoyi/jiande/JiandeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruoyi.jiande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiandeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
